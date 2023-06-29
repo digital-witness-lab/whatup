0444162705bf5ec36139d5c2b7758845c0ee725f
Micha Gorelick <mynameisfiber@gmail.com>
2023-06-29 16:07:27 +0200

    gofmt

--- whatupcore2/pkg/whatupcore2/whatupcoreserver.go
+++ whatupcore2/pkg/whatupcore2/whatupcoreserver.go
@@ -34,17 +34,17 @@ func (s *WhatUpCoreServer) GetMessages(messageOptions *pb.MessagesOptions, serve
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
-    defer cancel()
+	defer cancel()
 	msgChan := session.Client.GetMessages(ctx)
 
 	for msg := range msgChan {
-        if messageOptions.MarkMessagesRead {
-            msg.MarkRead()
-        }
-        msgProto, ok := msg.ToProto()
-        if !ok {
-            session.Client.Log.Errorf("Could not convert message to WUMessage proto: %v", msg)
-        } else if err := server.Send(msgProto); err != nil {
+		if messageOptions.MarkMessagesRead {
+			msg.MarkRead()
+		}
+		msgProto, ok := msg.ToProto()
+		if !ok {
+			session.Client.Log.Errorf("Could not convert message to WUMessage proto: %v", msg)
+		} else if err := server.Send(msgProto); err != nil {
 			return nil
 		}
 	}
@@ -58,10 +58,10 @@ func (s *WhatUpCoreServer) GetGroupInfo(ctx context.Context, pJID *pb.JID) (*pb.
 		return nil, status.Errorf(codes.FailedPrecondition, "Could not find session")
 	}
 
-    JID := ProtoToJID(pJID)
-    groupInfo, err := session.Client.GetGroupInfo(JID)
-    if err != nil {
-        return nil, status.Errorf(codes.Unknown, "%v", err)
-    }
-    return GroupInfoToProto(groupInfo), nil
+	JID := ProtoToJID(pJID)
+	groupInfo, err := session.Client.GetGroupInfo(JID)
+	if err != nil {
+		return nil, status.Errorf(codes.Unknown, "%v", err)
+	}
+	return GroupInfoToProto(groupInfo), nil
 }
