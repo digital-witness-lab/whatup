b607ff8adb1b5ae6990b25cbe4c63462a491a755
Micha Gorelick <mynameisfiber@gmail.com>
2023-09-22 12:11:14 +0200

    fixed inf. recursion bug in whatupcore2.utils:findRunAction(). created parallel redact script

---
+++ scripts/redact-message-archive.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# This script will take in a message archive and mirror it into a new archive
+# that has been run through `whatupcore2 redact`. It uses parallel to run and
+# keeps a joblog. In case of failures, you can re-run only failed redactions
+# using:
+# 
+# parallel --retry-failed --joblog ./<new-archive>/job.log
+
+redact_cmd="docker compose run -T whatup redact -"
+source_archive=$1
+target_archive=$2
+message_type=
+
+find_groupinfo='group-info_[0-9]+.json'
+find_wumessage='[0-9]+_[A-Z0-9]+\.json$'
+
+log_success=$target_archive/redact.success.log
+log_fail=$target_archive/redact.fail.log
+
+(
+for source in $( find "$source_archive" -type f -name \*.json ) ; do
+    target_rel=$( realpath --relative-to="$source_archive" "$source" )
+    target_parent=$target_archive/$( dirname "$target_rel" )
+    target=$target_archive/$target_rel
+
+    if [[ "$source" =~ $find_groupinfo ]]; then
+        message_type="GroupInfo"
+    elif [[ "$source" =~ $find_wumessage ]]; then
+        message_type="WUMessage"
+    else
+        # This will fail and will continue to fail if retrying through parallel's joblog.
+        message_type="XXXX"
+    fi
+    mkdir -p "${target_parent}"
+    echo -en "cat $source | $redact_cmd > $target\0"
+done;
+) | parallel --jobs "200%" --bar --null --joblog $target_archive/job.log
