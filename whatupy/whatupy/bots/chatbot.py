10d22cdce79bdc67096a77a5ebe7d0dec76ebe38
Archana Ahlawat <archana.ahlawat7@gmail.com>
2023-10-02 10:34:00 -0400

    use anon jid:jid dict

--- whatupy/whatupy/bots/chatbot.py
+++ whatupy/whatupy/bots/chatbot.py
@@ -8,7 +8,7 @@ from ..protos import whatupcore_pb2 as wuc
 from . import BaseBot
 
 logger = logging.getLogger(__name__)
-CHATBOT_FRIEND_JIDS: T.List[wuc.JID] = []
+CHATBOT_FRIEND_JIDS: T.Dict[str, wuc.JID] = {}
 
 class ChatBot(BaseBot):
     __version__ = "1.0.0"
@@ -26,8 +26,10 @@ class ChatBot(BaseBot):
         super().__init__(*args, **kwargs)
 
     async def start(self, **kwargs):
-        self.JID = (await self.core_client.GetConnectionStatus(wuc.ConnectionStatusOptions())).JID
-        CHATBOT_FRIEND_JIDS.append(self.JID)
+        self.strAnonJID = utils.jid_to_str((await self.core_client.GetConnectionStatus(wuc.ConnectionStatusOptions())).AnonJID)
+        JID = (await self.core_client.GetConnectionStatus(wuc.ConnectionStatusOptions())).JID
+        CHATBOT_FRIEND_JIDS.append(JID)
+        CHATBOT_FRIEND_JIDS[self.strAnonJID] = JID
         self.logger.info("Logged self as an active chatbot.")
         async with asyncio.TaskGroup() as tg:
             tg.create_task(super().start())
@@ -35,28 +37,27 @@ class ChatBot(BaseBot):
 
     async def initiate_friend_chats(self, *args, **kwargs):
         self.logger.info("Greeting friends")
-        for friend in CHATBOT_FRIEND_JIDS:
-            if not utils.same_jid(friend, self.JID): continue
-            self.logger.info(f"Saying hello to: {friend}")
+        for anon_friend in CHATBOT_FRIEND_JIDS:
+            if anon_friend == self.strAnonJID: continue
+            self.logger.info(f"Saying hello to: {CHATBOT_FRIEND_JIDS[anon_friend]}")
             await asyncio.sleep(5)
-            await self.send_text_message(friend, self.generate_message()) 
+            await self.send_text_message(CHATBOT_FRIEND_JIDS[anon_friend], self.generate_message()) 
 
     async def on_message(self, message: wuc.WUMessage, **kwargs):
         self.logger.info(f"Received a message")
         if message.info.source.isFromMe or message.info.source.isGroup:
             return
     
-        chat_JID: wuc.JID = message.info.source.chat
-        if chat_JID is None:
+        anon_chat_JID: str = utils.jid_to_str(message.info.source.chat)
+        if anon_chat_JID is None:
             self.logger.critical("Message has no chat_JID")
             return
-        if not any(utils.same_jid(chat_JID, friend) for friend in CHATBOT_FRIEND_JIDS):
+        if not anon_chat_JID in CHATBOT_FRIEND_JIDS.keys():
             return
-        str_JID = utils.jid_to_str(chat_JID)
-        if n_pending := self.pending_messages.get(str_JID):
+        if n_pending := self.pending_messages.get(anon_chat_JID):
             if random.random() > 1 / n_pending:
                 self.logger.info(
-                    f"Not responding to this message because there are too many queued for chat {chat_JID}: {n_pending}"
+                    f"Not responding to this message because there are too many queued for anon chat {anon_chat_JID}: {n_pending}"
                 )
                 return
         
@@ -64,14 +65,14 @@ class ChatBot(BaseBot):
         wait_time = max(
             1.0, random.normalvariate(self.response_time, self.response_time_sigma)
         )
-        self.logger.info(f"Sending message in {wait_time}seconds: {chat_JID}: {response}")
+        self.logger.info(f"Sending message in {wait_time}seconds: {anon_chat_JID}: {response}")
 
-        self.pending_messages.setdefault(str_JID, 0)
-        self.pending_messages[str_JID] += 1
+        self.pending_messages.setdefault(anon_chat_JID, 0)
+        self.pending_messages[anon_chat_JID] += 1
         await asyncio.sleep(wait_time)
-        self.pending_messages[str_JID] -= 1
+        self.pending_messages[anon_chat_JID] -= 1
 
-        status = await self.send_text_message(chat_JID, response)
+        status = await self.send_text_message(CHATBOT_FRIEND_JIDS[anon_chat_JID], response)
         self.logger.info(f"Send status: {status}")
         return
 
@@ -80,5 +81,5 @@ class ChatBot(BaseBot):
         return " ".join(utils.random_words(n_words))
     
     def stop(self):
-        CHATBOT_FRIEND_JIDS.remove(self.JID)
+        CHATBOT_FRIEND_JIDS.pop(self.strAnonJID)
         super().stop()
