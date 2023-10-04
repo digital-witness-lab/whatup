8e8b6912393bbf70dd42e1f1fa5afb60123a3921
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-10-04 11:18:59 +0530

    Flesh out the database and network configurations. Enable linting and formatting.

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -1,1 +1,54 @@
-# TODO: Create a Cloud SQL instance inside our VPC in a private subnet.
+from pulumi import Output, ResourceOptions
+
+from pulumi_gcp import sql
+
+from .config import db_name, db_password, db_user
+from .network import private_services_network, private_db_network, vpc
+
+sql_instance_settings = sql.DatabaseInstanceSettingsArgs(
+    tier="db-f1-micro",
+    deletion_protection_enabled=True,
+    ip_configuration=sql.DatabaseInstanceSettingsIpConfigurationArgs(
+        allocated_ip_range=private_db_network.name,
+        enable_private_path_for_google_cloud_services=True,
+        ipv4_enabled=False,
+        authorized_networks=[
+            sql.DatabaseInstanceSettingsIpConfigurationAuthorizedNetworkArgs(
+                name="private-services-network",
+                value=private_services_network.ip_cidr_range,
+            )
+        ],
+        private_network=vpc.id,
+    ),
+)
+
+cloud_sql_instance = sql.DatabaseInstance(
+    "sqlInstance",
+    database_version="POSTGRES_15",
+    settings=sql_instance_settings,
+    opts=ResourceOptions(protect=True),
+)
+
+database = sql.Database(
+    "database",
+    instance=cloud_sql_instance.name,
+    name=db_name,
+)
+
+sql.User(
+    "dbUser",
+    name=db_user,
+    instance=cloud_sql_instance.name,
+    password=db_password,
+)
+
+sql_instance_url = Output.concat(
+    "postgres://",
+    db_user,
+    ":",
+    db_password,
+    "@/",
+    db_name,
+    "?host=/cloudsql/",
+    cloud_sql_instance.connection_name,
+)
--- infrastructure/network.py
+++ infrastructure/network.py
@@ -1,2 +1,70 @@
-# TODO: Create a VPC with one public and a private subnet each for all the services and the DB.
-# We also need a serverless VPC connector for use with the CloudRun services.
+from pulumi_gcp import compute
+
+vpc = compute.Network("vpc", auto_create_subnetworks=False)
+
+private_services_network = compute.Subnetwork(
+    "privateServicesNetwork",
+    name="private-services",
+    network=vpc.id,
+    ip_cidr_range="10.1.0.0/16",
+    private_ip_google_access=True,
+)
+
+private_db_network = compute.Subnetwork(
+    "dbNetwork",
+    name="db",
+    network=vpc.id,
+    ip_cidr_range="10.2.0.0/28",
+    private_ip_google_access=True,
+)
+
+firewall_policy = compute.NetworkFirewallPolicy(
+    "vpcFirewallPolicy",
+    description="VPC firewall policy",
+)
+
+compute.NetworkFirewallPolicyRule(
+    "dbFirewallRule",
+    compute.NetworkFirewallPolicyRuleArgs(
+        action="allow",
+        description="Allow private services to connect to the DB",
+        direction="INGRESS",
+        disabled=False,
+        enable_logging=True,
+        firewall_policy=firewall_policy.name,
+        priority=1000,
+        rule_name="allow-db-connectivity-rule",
+        match=compute.NetworkFirewallPolicyRuleMatchArgs(
+            src_ip_ranges=[private_services_network.ip_cidr_range],
+            layer4_configs=[
+                compute.NetworkFirewallPolicyRuleMatchLayer4ConfigArgs(
+                    ip_protocol="all",
+                )
+            ],
+            dest_ip_ranges=[private_db_network.ip_cidr_range],
+        ),
+    ),
+)
+
+compute.NetworkFirewallPolicyRule(
+    "internetEgressFirewallRule",
+    compute.NetworkFirewallPolicyRuleArgs(
+        action="allow",
+        description="Allow private services to connect to the internet",
+        direction="INGRESS",
+        disabled=False,
+        enable_logging=True,
+        firewall_policy=firewall_policy.name,
+        priority=1001,
+        rule_name="allow-db-connectivity-rule",
+        match=compute.NetworkFirewallPolicyRuleMatchArgs(
+            src_ip_ranges=[private_services_network.ip_cidr_range],
+            layer4_configs=[
+                compute.NetworkFirewallPolicyRuleMatchLayer4ConfigArgs(
+                    ip_protocol="all",
+                )
+            ],
+            dest_ip_ranges=["0.0.0.0/0"],
+        ),
+    ),
+)
