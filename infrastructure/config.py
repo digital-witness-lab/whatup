b8e585b114b1a0020f18fbe660f3c923367c5a37
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-10-04 14:42:56 +0530

    Create storage buckets for all services. Create bucket permissions for whatupcore2.

--- infrastructure/config.py
+++ infrastructure/config.py
@@ -1,9 +1,10 @@
+from typing import List
 import pulumi
 
 config = pulumi.Config()
-db_name = config.require("dbName")
+db_names: List[str] = config.require_object("dbNames")
 db_password = config.require_secret("dbPassword")
-db_user = config.require("dbUser")
+db_root_password = config.require_secret("dbRootPassword")
 
 
 # Import the provider's configuration settings.
