b64074cc76ea38116136d9804292a1a8fe7df424
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-10-06 15:41:04 +0530

    Add a flag to control execution of onboard bulk job

--- infrastructure/jobs/bot_onboard_bulk.py
+++ infrastructure/jobs/bot_onboard_bulk.py
@@ -3,6 +3,7 @@ from os import path
 from pulumi import ResourceOptions
 from pulumi_gcp import serviceaccount, cloudrunv2, storage
 
+from ..config import create_onboard_bulk_job
 from ..job import JobArgs, Job
 from ..network import vpc, private_services_network
 from ..storage import sessions_bucket
@@ -23,40 +24,41 @@ sessions_bucket_perm = storage.BucketIAMMember(
     ),
 )
 
-bot_onboard_bulk_job = Job(
-    service_name,
-    JobArgs(
-        app_path=path.join("..", "..", "whatupy"),
-        commands=[
-            "onboard-bulk",
-            "--host",
-            "$WHATUPCORE2_HOST",
-            "--credentials-dir",
-            "/usr/src/whatupy-data/sessions/",
-        ],
-        concurrency=50,
-        cpu="1",
-        # Route all egress traffic via the VPC network.
-        egress="ALL_TRAFFIC",
-        image_name=service_name,
-        # We want this service to only be reachable from within
-        # our VPC network.
-        ingress="INGRESS_TRAFFIC_INTERNAL_ONLY",
-        memory="1Gi",
-        service_account=service_account,
-        # Specifying the subnet causes CloudRun to use
-        # Direct VPC egress for outbound traffic based
-        # on the value of the `egress` property above.
-        subnet=cloudrunv2.JobTemplateTemplateVpcAccessNetworkInterfaceArgs(
-            network=vpc.id, subnetwork=private_services_network.id
-        ),
-        envs=[
-            cloudrunv2.JobTemplateTemplateContainerEnvArgs(
-                name="SESSIONS_BUCKET",
-                value=sessions_bucket.name,
+if create_onboard_bulk_job:
+    bot_onboard_bulk_job = Job(
+        service_name,
+        JobArgs(
+            app_path=path.join("..", "..", "whatupy"),
+            commands=[
+                "onboard-bulk",
+                "--host",
+                "$WHATUPCORE2_HOST",
+                "--credentials-dir",
+                "/usr/src/whatupy-data/sessions/",
+            ],
+            concurrency=50,
+            cpu="1",
+            # Route all egress traffic via the VPC network.
+            egress="ALL_TRAFFIC",
+            image_name=service_name,
+            # We want this service to only be reachable from within
+            # our VPC network.
+            ingress="INGRESS_TRAFFIC_INTERNAL_ONLY",
+            memory="1Gi",
+            service_account=service_account,
+            # Specifying the subnet causes CloudRun to use
+            # Direct VPC egress for outbound traffic based
+            # on the value of the `egress` property above.
+            subnet=cloudrunv2.JobTemplateTemplateVpcAccessNetworkInterfaceArgs(
+                network=vpc.id, subnetwork=private_services_network.id
             ),
-        ],
-        timeout="3600s",
-    ),
-    opts=ResourceOptions(depends_on=sessions_bucket_perm),
-)
+            envs=[
+                cloudrunv2.JobTemplateTemplateContainerEnvArgs(
+                    name="SESSIONS_BUCKET",
+                    value=sessions_bucket.name,
+                ),
+            ],
+            timeout="3600s",
+        ),
+        opts=ResourceOptions(depends_on=sessions_bucket_perm),
+    )
