07e5f3525ee6f6c7b304b8144f60c9f56fa99422
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-10-06 15:41:04 +0530

    Add a flag to control execution of onboard bulk job

--- infrastructure/config.py
+++ infrastructure/config.py
@@ -6,6 +6,8 @@ db_names: List[str] = config.require_object("dbNames")
 db_password = config.require_secret("dbPassword")
 db_root_password = config.require_secret("dbRootPassword")
 
+create_onboard_bulk_job = config.get_bool("createOnboardBulkJob")
+
 
 # Import the provider's configuration settings.
 gcp_config = pulumi.Config("gcp")
