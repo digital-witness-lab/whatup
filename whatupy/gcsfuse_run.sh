e4138513bf8a357d19336d2ae3169cde6914b6e6
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-10-12 16:32:11 +0530

    Enable automatic DB backups. Disable gcsfuse debug logging

--- whatupy/gcsfuse_run.sh
+++ whatupy/gcsfuse_run.sh
@@ -4,12 +4,12 @@ set -eo pipefail
 echo "Mounting $SESSIONS_BUCKET bucket using GCS Fuse."
 # Create mount directory for service
 mkdir -p "${BUCKET_MNT_DIR_PREFIX}/${SESSIONS_BUCKET_MNT_DIR}"
-gcsfuse --debug_gcs --debug_fuse "$SESSIONS_BUCKET" "${BUCKET_MNT_DIR_PREFIX}/${SESSIONS_BUCKET_MNT_DIR}"
+gcsfuse "$SESSIONS_BUCKET" "${BUCKET_MNT_DIR_PREFIX}/${SESSIONS_BUCKET_MNT_DIR}"
 
 if [ -n "${MESSAGE_ARCHIVE_BUCKET:-}" ]; then
     echo "Mounting $MESSAGE_ARCHIVE_BUCKET bucket using GCS Fuse."
     mkdir -p "${BUCKET_MNT_DIR_PREFIX}/${MESSAGE_ARCHIVE_BUCKET_MNT_DIR}"
-    gcsfuse --debug_gcs --debug_fuse "$MESSAGE_ARCHIVE_BUCKET" "${BUCKET_MNT_DIR_PREFIX}/${MESSAGE_ARCHIVE_BUCKET_MNT_DIR}"
+    gcsfuse "$MESSAGE_ARCHIVE_BUCKET" "${BUCKET_MNT_DIR_PREFIX}/${MESSAGE_ARCHIVE_BUCKET_MNT_DIR}"
 fi
 
 echo "Mounting completed."
