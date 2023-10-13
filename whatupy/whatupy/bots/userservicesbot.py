fc9e8aa934ecf580ae3d68f3aefc599c6f5c89d0
Micha Gorelick <mynameisfiber@gmail.com>
2023-10-13 17:30:46 -0400

    working through idea of device manager

--- whatupy/whatupy/bots/userservicesbot.py
+++ whatupy/whatupy/bots/userservicesbot.py
@@ -1,35 +1,29 @@
 import json
 from pathlib import Path
+import asyncio
 import typing as T
+import logging
 
 import qrcode
 import dataset
 
 from .. import NotRegisteredError, utils
-from . import BaseBot
+from . import BaseBot, BotType
 from . import BotCommandArgs, MediaType
 from ..protos import whatupcore_pb2 as wuc
 from ..connection import WhatUpAuthentication
 
 
-class UserClient(BaseBot):
-    pass
-
-class BotManager:
-    def __init__(self, bot_class):
-        # create this bot that does what run_multi_bots and what the self.users
-        # in UserServicesBot. This should be able to lazy-instantiate on
-        # session dir
-        pass
+logger = logging.getLogger(__name__)
 
 
-class UserServicesBot(BaseBot):
+class UserServicesBot(DeviceManager, BaseBot):
     def __init__(self, sessions_dir: Path, database_url: str, *args, **kwargs):
         self.sessions_dir = sessions_dir
         self.db: dataset.Database = dataset.connect(database_url)
         self.users: T.Dict[str, UserClient] = {}
         super().__init__(*args, **kwargs)
 
-    def listen_new_sessions():
+    def listen_new_sessions(self):
         # look for new files in the session directory that arent currently
         # loaded
