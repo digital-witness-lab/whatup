cf75efa0123b66f774fc4dff4cd9ca95eea736a8
Archana Ahlawat <archana.ahlawat7@gmail.com>
2023-11-15 15:12:40 -0500

    make changes so that group info properly gets ephemeral / timer status

--- whatupcore2/pkg/whatupcore2/whatupcoreserver.go
+++ whatupcore2/pkg/whatupcore2/whatupcoreserver.go
@@ -323,7 +323,7 @@ func (s *WhatUpCoreServer) GetGroupInfo(ctx context.Context, pJID *pb.JID) (*pb.
 	groupInfoProto := GroupInfoToProto(groupInfo, session.Client.Store)
 	groupInfoProto.IsCommunityDefaultGroup = groupInfo.IsAnnounce
 	if groupInfo.IsAnnounce {
-		groupInfoProto.GroupTopic = &pb.GroupTopic{}
+		groupInfoProto.GroupTopic = nil//&pb.GroupTopic{}
 	}
 	return AnonymizeInterface(session.Client.anonLookup, groupInfoProto), nil
 }
