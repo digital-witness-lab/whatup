0fcc106a4679426fcb6c27c646ff6790d3628904
Archana Ahlawat <archana.ahlawat7@gmail.com>
2023-12-20 16:02:55 -0500

    can upload hashed image jsons to a new bucket for vertex indexing

--- hash-gen/hash_gen/localtesting.py
+++ hash-gen/hash_gen/localtesting.py
@@ -6,6 +6,7 @@ import imagehash
 import json
 from cloudpathlib import CloudPath, AnyPath
 from google.cloud import storage
+from google.cloud.storage import Client, transfer_manager
 import click
 
 image_folder = "/Users/aa3576/Desktop/Whatsapp-images-test/"
@@ -15,29 +16,37 @@ cloud_object = AnyPath('gs://whatup-message-archive/120363166051808934@g.us/medi
 local_object = AnyPath('/Users/aa3576/Desktop/Whatsapp-images/sim1.jpg')
 hash = imagehash.phash(Image.open(local_object.open("rb")))
 
+# this function takes a file or directory, local or gcloud bucket object, hashes it using phash, 
+# and then uploads a json file per image to dwl-vertex, our bucket for vertex indexing.
 @click.command()
 @click.argument(
     "file-or-dir", type=click.Path(path_type=AnyPath)
 )
-def hash_images(file_or_dir):
+def hash_images(file_or_dir): # can make bucket an argument later 
     file_or_dir = AnyPath(file_or_dir)
     files = []
     hashes = [] 
-    if file_or_dir.is_dir():
+    storage_client = Client()
+    bucket = storage_client.bucket("dwl-vertex")
+
+    if file_or_dir.is_dir(): # or can use rglob? test later
         files.extend(file_or_dir.glob("*"))
     else:
         files.append(file_or_dir)
+    i = 1
     for file in files:
         file = AnyPath(file)
         if not file.name.startswith('.'):
             hash = 1 * imagehash.phash(Image.open(file.open("rb"))).hash.flatten()
             hashes.append(hash)
-            hash_file = AnyPath("/Users/aa3576/Desktop/Whatsapp-images-jsons/" + f"{file.stem}.json")
-            entry = {"id": file.name, "embedding": hash.tolist()}
-            with hash_file.open("w",encoding='utf8') as fd:
-                fd.write(json.dumps(entry))
 
-    print(hashes)
+            blob = bucket.blob(f"{file.stem}.json")
+            entry = {"id": file.name, "embedding": hash.tolist()}
+            with blob.open("w",encoding='utf8') as fd: 
+                fd.write(json.dumps(entry))
+        i+= 1
+        if i == 100: return
+    # print(hashes)
 
 if __name__ == '__main__':
     hash_images()
