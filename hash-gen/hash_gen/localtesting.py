dff4d8a68d1684e5db34ab5def14e688726758f9
Archana Ahlawat <archana.ahlawat7@gmail.com>
2023-12-20 13:52:24 -0500

    formatted hash jsons for vertex + prelim vertex config

--- hash-gen/hash_gen/localtesting.py
+++ hash-gen/hash_gen/localtesting.py
@@ -1,7 +1,6 @@
 '''
 This is just for local testing. Cannot run through docker, run as a normal python file. 
 '''
-
 from PIL import Image
 import imagehash
 import json
@@ -31,16 +30,14 @@ def hash_images(file_or_dir):
     for file in files:
         file = AnyPath(file)
         if not file.name.startswith('.'):
-            hash = imagehash.phash(Image.open(file.open("rb")))
-            hashes.append(hash.hash)
-            hash_file = AnyPath("/Users/aa3576/Desktop/Whatsapp-images/" + f"{file.name}.json")
-            entry = {"id": file.name, "embedding": str(hash)}
-            with hash_file.open("w") as fd:
+            hash = 1 * imagehash.phash(Image.open(file.open("rb"))).hash.flatten()
+            hashes.append(hash)
+            hash_file = AnyPath("/Users/aa3576/Desktop/Whatsapp-images-jsons/" + f"{file.stem}.json")
+            entry = {"id": file.name, "embedding": hash.tolist()}
+            with hash_file.open("w",encoding='utf8') as fd:
                 fd.write(json.dumps(entry))
 
     print(hashes)
 
 if __name__ == '__main__':
     hash_images()
-
-# [int(ch) for ch in str(hash).split()] 
