dcb86c13d92d21d12c56557dac36c71f06499ffb
Archana Ahlawat <archana.ahlawat7@gmail.com>
2023-12-21 12:58:02 -0500

    script can take images in media directories from the media bucket (or locally), hash them, and create new hash jsons to add to the new vertex bucket

--- hash-gen/hash_gen/localtesting.py
+++ hash-gen/hash_gen/localtesting.py
@@ -13,8 +13,8 @@ image_folder = "/Users/aa3576/Desktop/Whatsapp-images-test/"
 
 # Simple example
 cloud_object = AnyPath('gs://whatup-message-archive/120363166051808934@g.us/media/0734mfFbmQCsIRppp-YqY5DDCSG-a1VqVuCz2ApzGPs=.jpg')
-local_object = AnyPath('/Users/aa3576/Desktop/Whatsapp-images/sim1.jpg')
-hash = imagehash.phash(Image.open(local_object.open("rb")))
+#local_object = AnyPath('/Users/aa3576/Desktop/Whatsapp-images/sim1.jpg')
+#hash = imagehash.phash(Image.open(local_object.open("rb")))
 
 # this function takes a file or directory, local or gcloud bucket object, hashes it using phash, 
 # and then uploads a json file per image to dwl-vertex, our bucket for vertex indexing.
@@ -22,31 +22,45 @@ hash = imagehash.phash(Image.open(local_object.open("rb")))
 @click.argument(
     "file-or-dir", type=click.Path(path_type=AnyPath)
 )
-def hash_images(file_or_dir): # can make bucket an argument later 
+def hash_images(file_or_dir): # can make bucket/local an argument later 
     file_or_dir = AnyPath(file_or_dir)
     files = []
     hashes = [] 
     storage_client = Client()
     bucket = storage_client.bucket("dwl-vertex")
 
-    if file_or_dir.is_dir(): 
-        files.extend(file_or_dir.rglob("*"))
-        files = [path for path in files if path.is_file()]
+    # below block could maybe be more efficient? i think this is the block that takes a while. 
+    if file_or_dir.is_dir(): # are local directory structures going to mirror the bucket? 
+        media_dirs = list(file_or_dir.rglob("*/media"))
+        for obj in media_dirs:
+            files.extend(AnyPath(obj).rglob("*"))
+        files = [path for path in files if path.is_file() and not path.name.startswith('.')]
     else:
         files.append(file_or_dir)
     i = 0
     for file in files:
         file = AnyPath(file)
-        if not file.name.startswith('.'):
+        try:
+            blob = bucket.blob(f"{file.stem}.json")
+            if blob.exists(): continue
+            
             hash = 1 * imagehash.phash(Image.open(file.open("rb"))).hash.flatten()
             hashes.append(hash)
-
-            blob = bucket.blob(f"{file.stem}.json")
             entry = {"id": file.name, "embedding": hash.tolist()}
+
             with blob.open("w",encoding='utf8') as fd: 
                 fd.write(json.dumps(entry))
-        i+= 1
-        if i == 100: return
+
+            # write to local folder for testing
+            # hash_file = AnyPath("/Users/aa3576/Desktop/Whatsapp-images-jsons/" + f"{file.stem}.json")
+            # entry = {"id": file.name, "embedding": hash.tolist()}
+            # with hash_file.open("w",encoding='utf8') as fd:
+            #     fd.write(json.dumps(entry))
+
+            i+= 1
+            if i == 5: return
+        except:
+            print("Skipping a non-image file.")
     print(len(hashes))
     print(i)
 
