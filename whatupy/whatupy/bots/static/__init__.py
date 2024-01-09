fd89678b16f0b6e0779afe776c5d4313150aef9c
Micha Gorelick <mynameisfiber@gmail.com>
2024-01-09 11:43:52 +0100

    Added preview card to group selection link, fixed wrong control groups for test/dev

--- whatupy/whatupy/bots/static/__init__.py
+++ whatupy/whatupy/bots/static/__init__.py
@@ -5,6 +5,7 @@ static_dir = Path(__file__).parent.absolute()
 static_files = {
     "group_selection": static_dir / "group-selection.html",
     "unregister_final_message": static_dir / "unregister_final_message.txt",
+    "group_selection_thumbnail": static_dir / "group-selection-thumbnail.jpg",
 }
 
 
