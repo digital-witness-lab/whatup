7353e07e3ed5e9595536f2c25bb61839e9219f05
Micha Gorelick <mynameisfiber@gmail.com>
2024-01-12 11:59:14 +0100

    restructured templates and readded lang selection from html

--- whatupy/whatupy/bots/static/__init__.py
+++ whatupy/whatupy/bots/static/__init__.py
@@ -1,14 +1,26 @@
 from pathlib import Path
 import string
+import typing as T
+
+import yaml
+
 
 static_dir = Path(__file__).parent.absolute()
+template_dir = Path(__file__).parent.absolute() / "templates/"
+
 static_files = {file.stem: file for file in static_dir.glob("*")}
+template_files = {file.stem: file for file in template_dir.glob("*")}
 
 
 class Template(string.Template):
     delimiter = "#"
 
 
-def format_template(template_name, **kwargs) -> str:
-    template = Template(static_files[template_name].open().read())
-    return template.safe_substitute(kwargs)
+def substitute(message, **kwargs):
+    return Template(message).safe_substitute(**kwargs)
+
+
+def format_lang_template(template_name, lang, **kwargs) -> T.List[str]:
+    data = template_files[template_name].read_text("utf8")
+    messages = yaml.safe_load(data)[lang]
+    return [substitute(message, **kwargs) for message in messages]
