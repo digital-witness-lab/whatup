c34c80f6605854aec0c0795e016c1ee643a9f48a
Archana Ahlawat <archana.ahlawat7@gmail.com>
2024-01-31 16:12:53 -0800

    wip phash table in db

--- hash-gen/hash_gen/phash_images.py
+++ hash-gen/hash_gen/phash_images.py
@@ -4,13 +4,30 @@ import json
 from cloudpathlib import CloudPath, AnyPath
 from google.cloud import storage
 import click
+import dataset
+from sqlalchemy import types
+
+def db_setup(database_url):
+    print(database_url)
+    db: dataset.Database = dataset.connect(database_url)
+    phash_table = db.create_table(
+        "phash_images",
+        primary_id="filename",
+        primary_type=db.types.text,
+        primary_increment=False
+    )
+    phash_table.create_column('phash', types.ARRAY(db.types.integer))
+    return db
 
 # Process image files or directories
 @click.command()
+@click.option("--database-url", type=str)
 @click.argument(
     "file-or-dir", type=click.Path(path_type=AnyPath)
 )
-def hash_images(file_or_dir):
+def hash_images(file_or_dir, database_url):
+    db = db_setup(database_url)
+
     file_or_dir = AnyPath(file_or_dir)
     files = []
     hashes = [] 
@@ -33,10 +50,9 @@ def hash_images(file_or_dir):
             
             hash = 1 * imagehash.phash(Image.open(file.open("rb"))).hash.flatten()
             hashes.append(hash)
-            entry = {"id": file.name, "embedding": hash.tolist()} # UPDATE TO DB ENTRY
-
-            # INSERT IN DB
-
+            entry = {"filename": file.name, "phash": hash.tolist()}
+            
+            db["phash_images"].insert(entry)
             i+= 1
             if i == 5: return
         except:
