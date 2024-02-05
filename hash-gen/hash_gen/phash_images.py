246af3d9b559e062f9c8949cc3c7b0245eee1998
Archana Ahlawat <archana.ahlawat7@gmail.com>
2024-02-05 15:46:24 -0800

    hash_images takes images from a given bucket, hashes them, and adds them to phash_images bigquery table

--- hash-gen/hash_gen/phash_images.py
+++ hash-gen/hash_gen/phash_images.py
@@ -8,7 +8,10 @@ import click
 #from sqlalchemy import types
 from google.cloud import bigquery
 
-# Process image files or directories
+
+cloud_object = AnyPath('gs://whatup-message-archive') # Can use this as argument for hash_images. 
+
+# Process local or cloud image files or directories
 @click.command()
 @click.argument(
     "file-or-dir", type=click.Path(path_type=AnyPath)
@@ -22,40 +25,47 @@ def hash_images(file_or_dir):
         bigquery.SchemaField("phash", "BYTES", mode="REQUIRED"),
     ]
 
+    # table = bigquery.Table(table_id, schema=schema)
+    # table = client.create_table(table) 
+
     file_or_dir = AnyPath(file_or_dir)
-    files = []
-    hashes = [] 
+    files = {}
 
-    if file_or_dir.is_dir(): # are local directory structures going to mirror the bucket? 
-        media_dirs = list(file_or_dir.rglob("*/media"))
+    if file_or_dir.is_dir():
+        print("start")
+        media_dirs = list(file_or_dir.rglob("*/media")) # takes ~ 30 secs - 1 min locally given 36 media dirs as of 2.5.24
+        #media_dirs = list(file_or_dir.rglob("media")) # use this if directly pointing to one media directory
         for obj in media_dirs:
-            files.extend(AnyPath(obj).rglob("*"))
-        files = [path for path in files if path.is_file() and not path.name.startswith('.')]
+            files[obj] = list(AnyPath(obj).rglob("*"))
     else:
-        files.append(file_or_dir)
+        files[file_or_dir] = [file_or_dir]
     i = 0
-    for file in files:
-        file = AnyPath(file)
+    # run through every key and its list of files. This took ~45 mins.
+    for media_dir in files:
+        new_entries = []
+        for file in files[media_dir]:
+            if not file.is_file() or file.name.startswith('.'): continue
+            file = AnyPath(file)
 
-        try:
-            QUERY = ('SELECT filename FROM `{}` WHERE filename = "{}" ').format(table_id, file.name)
-            query_job = client.query(QUERY) 
-            rows = query_job.result()  
-            if rows.total_rows > 0: break # hash already exists
+            try:
+                QUERY = ('SELECT filename FROM `{}` WHERE filename = "{}"').format(table_id, file.name)
+                query_job = client.query(QUERY) 
+                rows = query_job.result()  
+                if rows.total_rows > 0: break # hash already exists
 
-            hash = str(imagehash.phash(Image.open(file.open("rb"))))
-            byte_hash = bytes.fromhex(hash)
+                hash = str(imagehash.phash(Image.open(file.open("rb"))))
+                byte_hash = bytes.fromhex(hash)
 
-            entry = [{"filename": file.name, "phash": byte_hash}]
-            errors = client.insert_rows(table_id, entry, schema)
-            if errors == []:
-                i+= 1
-            else:
-                print("Encountered errors while inserting rows: {}".format(errors))
-        except:
-            print("Skipping a non-image file.")
-
-    print(len(hashes))
+                new_entries.append({"filename": file.name, "phash": byte_hash})
+            except:
+                print("Skipping a non-image file.")
+        if len(new_entries) > 0:
+            errors = client.insert_rows(table_id, new_entries, schema)
+            if errors == []: 
+                i += len(new_entries)
+                print("Added {} new rows".format(len(new_entries)))
+            else: print("Encountered errors while inserting rows: {}".format(errors))
+        
     print(i)
 
 if __name__ == '__main__':
