fff7b6701b3075f17999fe1edb794261a72213a7
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-02-11 16:23:50 -0800

    WIP comment out some things for dev

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -3,21 +3,18 @@ import pulumi
 
 # Import this first to register the services first.
 import gcp_services
-from bigquery import bigquery_sql_connection
-from jobs import (
-    bot_db_load_archive,
-    bot_onboard_bulk,
-    bq_init_schema,
-    db_migrations,
-    db_delete_groups,
-    whatupcore_remove_user,
-)
+
+# from bigquery import bigquery_sql_connection
+# from jobs import (
+#     bot_db_load_archive,
+#     bot_onboard_bulk,
+#     bq_init_schema,
+#     db_migrations,
+#     db_delete_groups,
+#     whatupcore_remove_user,
+# )
 from services import (
-    bot_archive,
-    bot_db,
-    bot_register,
-    bot_user_services,
     whatupcore2,
 )
 
-pulumi.export("bigquery_connection_id", bigquery_sql_connection.connection_id)
+# pulumi.export("bigquery_connection_id", bigquery_sql_connection.connection_id)
