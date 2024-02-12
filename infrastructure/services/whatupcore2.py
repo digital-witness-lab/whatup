9f2613c2489a9e7854e101dfbcb8bdf61a778594
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-02-11 16:23:25 -0800

    Convert whatupcore2 to use container-on-vm compute

--- infrastructure/services/whatupcore2.py
+++ infrastructure/services/whatupcore2.py
@@ -1,5 +1,6 @@
 from pulumi import Output, ResourceOptions, get_stack
 from pulumi_gcp import cloudrunv2, secretmanager, serviceaccount
+from pulumi_google_native import compute
 from pulumi_gcp.cloudrunv2 import (
     ServiceTemplateContainerEnvValueSourceSecretKeyRefArgs,
 )  # noqa: E501
@@ -11,8 +12,15 @@ from dwl_secrets import (
     whatup_anon_key_secret,
     whatup_salt_secret,
 )
-from network import private_services_network_with_db, vpc
-from service import Service, ServiceArgs
+from network import private_services_network_with_db
+from container_vm import (
+    ContainerOnVm,
+    ContainerOnVmArgs,
+    ContainerSpec,
+    ContainerEnv,
+    SharedCoreMachineType,
+    ContainerSecurityContext,
+)
 
 service_name = "whatupcore2"
 
@@ -75,59 +83,50 @@ whatup_anon_key_secret_source = (
 )
 
 log_level = "INFO"  # if is_prod_stack() else "DEBUG"
-whatupcore2_service = Service(
+whatupcore2_service = ContainerOnVm(
     service_name,
-    ServiceArgs(
-        args=["rpc", f"--log-level={log_level}"],
-        concurrency=500,
-        container_port=3447,
-        cpu="1",
-        # Route only egress traffic bound for private IPs
-        # via the VPC network. All other traffic will take
-        # the default route bound for the internet gateway.
-        # Routing all traffic via the VPC for this container
-        # will cause the websocket connection to WhatsApp to
-        # fail due to dial timeout.
-        egress="PRIVATE_RANGES_ONLY",
-        image=whatupcore2_image,
-        # We want this service to only be reachable from within
-        # our VPC network.
-        ingress="INGRESS_TRAFFIC_INTERNAL_ONLY",
-        memory="1Gi",
-        public_access=True,
-        service_account=service_account,
-        # Specifying the subnet causes CloudRun to use
-        # Direct VPC egress for outbound traffic based
-        # on the value of the `egress` property above.
-        subnet=cloudrunv2.ServiceTemplateVpcAccessNetworkInterfaceArgs(
-            network=vpc.id, subnetwork=private_services_network_with_db.id
+    ContainerOnVmArgs(
+        container_spec=ContainerSpec(
+            command=["rpc"],
+            args=[f"--log-level={log_level}"],
+            image=whatupcore2_image.repo_digest,
+            env=[
+                ContainerEnv(
+                    name="USE_SSL",
+                    value="false",
+                ),
+                ContainerEnv(
+                    name="APP_NAME_SUFFIX",
+                    value="" if is_prod_stack() else get_stack(),
+                ),
+                ContainerEnv(
+                    name="RAND_STRING",  # change rand string to force deploy
+                    value="34943534473298",
+                ),
+            ],
+            restartPolicy="Always",
+            tty=False,
+            securityContext=ContainerSecurityContext(privileged=False),
         ),
-        envs=[
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="USE_SSL",
-                value="false",
+        machine_type=SharedCoreMachineType.E2Small,
+        secret_env=[
+            compute.v1.MetadataItemsItemArgs(
+                key="DATABASE_URL",
+                value=Output.concat(
+                    db_url_secrets["whatupcore"].id, "/latest"
+                ),
             ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="DATABASE_URL",
-                value_source=db_url_secret_source,
+            compute.v1.MetadataItemsItemArgs(
+                key="ENC_KEY_SALT",
+                value=Output.concat(whatup_salt_secret.id, "/latest"),
             ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="APP_NAME_SUFFIX",
-                value="" if is_prod_stack() else get_stack(),
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="ENC_KEY_SALT",
-                value_source=whatup_salt_secret_source,
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="ANON_KEY",
-                value_source=whatup_anon_key_secret_source,
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="RAND_STRING",  # change rand string to force deploy
-                value="34943534473298",
+            compute.v1.MetadataItemsItemArgs(
+                key="ANON_KEY",
+                value=Output.concat(whatup_anon_key_secret.id, "/latest"),
             ),
         ],
+        service_account_email=service_account.email,
+        subnet=private_services_network_with_db.self_link,
     ),
     opts=ResourceOptions(
         depends_on=[
