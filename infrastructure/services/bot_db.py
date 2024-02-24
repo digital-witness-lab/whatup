dc5072a3e2830d1486ace3e782aa1624cb76e335
Micha Gorelick <mynameisfiber@gmail.com>
2024-02-24 17:23:59 +0100

    migrated bot_db to VM

--- infrastructure/services/bot_db.py
+++ infrastructure/services/bot_db.py
@@ -1,25 +1,30 @@
 from pulumi import Output, ResourceOptions, get_stack
-from pulumi_gcp import cloudrunv2, kms, secretmanager, serviceaccount, storage
-from pulumi_gcp.cloudrunv2 import (
-    ServiceTemplateContainerEnvValueSourceSecretKeyRefArgs,
-)  # noqa: E501
+from pulumi_gcp import kms, secretmanager, serviceaccount, storage
+from pulumi_google_native import compute
 
 from artifact_registry import whatupy_image
-from config import control_groups
+from config import control_groups, is_prod_stack
 from dwl_secrets import db_url_secrets
 from jobs.db_migrations import migrations_job_complete
 from kms import sessions_encryption_key, sessions_encryption_key_uri
-from network import private_services_network_with_db, vpc
-from service import Service, ServiceArgs
+from network import private_services_network_with_db
 from storage import media_bucket, sessions_bucket
+from container_vm import (
+    Container,
+    ContainerEnv,
+    ContainerOnVm,
+    ContainerOnVmArgs,
+    ContainerSecurityContext,
+    SharedCoreMachineType,
+)
 
-from .whatupcore2 import ssl_cert_pem_b64_secret, whatupcore2_service
+from .whatupcore2 import ssl_cert_pem_secret, whatupcore2_service
 
 service_name = "bot-db"
 
 service_account = serviceaccount.Account(
     "bot-db",
-    account_id=f"whatup-bot-db-{get_stack()}",
+    account_id=f"whatup-bot-db-vm-{get_stack()}",
     description=f"Service account for {service_name}",
 )
 
@@ -61,92 +66,82 @@ encryption_key_perm = kms.CryptoKeyIAMMember(
     ),
 )
 
-db_url_secret_source = cloudrunv2.ServiceTemplateContainerEnvValueSourceArgs(
-    secret_key_ref=ServiceTemplateContainerEnvValueSourceSecretKeyRefArgs(
-        secret=db_url_secrets["messages"].name,
-        version="latest",
-    )
-)
-
-ssl_cert_pem_b64_secret_perm = secretmanager.SecretIamMember(
+ssl_cert_pem_secret_perm = secretmanager.SecretIamMember(
     "bot-db-ssl-cert-secret-perm",
     secretmanager.SecretIamMemberArgs(
-        secret_id=ssl_cert_pem_b64_secret.id,
+        secret_id=ssl_cert_pem_secret.id,
         role="roles/secretmanager.secretAccessor",
         member=Output.concat("serviceAccount:", service_account.email),
     ),
 )
-ssl_cert_pem_b64_source = cloudrunv2.ServiceTemplateContainerEnvValueSourceArgs(
-    secret_key_ref=cloudrunv2.ServiceTemplateContainerEnvValueSourceSecretKeyRefArgs(
-        secret=ssl_cert_pem_b64_secret.name,
-        version="latest",
-    ),
-)
 
-bot_db = Service(
+machine_type = SharedCoreMachineType.E2Medium
+if not is_prod_stack():
+    machine_type = SharedCoreMachineType.E2Micro
+
+bot_db = ContainerOnVm(
     service_name,
-    ServiceArgs(
-        args=["/usr/src/whatupy/run.sh", "databasebot"],
-        concurrency=50,
-        container_port=None,
-        cpu="1",
-        # Route all egress traffic via the VPC network.
-        egress="ALL_TRAFFIC",
-        image=whatupy_image,
-        # We want this service to only be reachable from within
-        # our VPC network.
-        ingress="INGRESS_TRAFFIC_INTERNAL_ONLY",
-        memory="1Gi",
-        public_access=True,
-        service_account=service_account,
-        # Specifying the subnet causes CloudRun to use
-        # Direct VPC egress for outbound traffic based
-        # on the value of the `egress` property above.
-        subnet=cloudrunv2.ServiceTemplateVpcAccessNetworkInterfaceArgs(
-            network=vpc.id, subnetwork=private_services_network_with_db.id
+    ContainerOnVmArgs(
+        automatic_static_private_ip=False,
+        container_spec=Container(
+            args=["/usr/src/whatupy/run.sh", "databasebot"],
+            image=whatupy_image.repo_digest,
+            env=[
+                ContainerEnv(
+                    name="KEK_URI",
+                    value=sessions_encryption_key_uri,
+                ),
+                ContainerEnv(
+                    name="SESSIONS_BUCKET",
+                    value=sessions_bucket.name,
+                ),
+                ContainerEnv(
+                    name="MEDIA_BUCKET",
+                    value=media_bucket.name,
+                ),
+                ContainerEnv(
+                    name="WHATUPY_CONTROL_GROUPS",
+                    value=" ".join(control_groups),
+                ),
+                ContainerEnv(
+                    name="WHATUPCORE2_HOST",
+                    value=whatupcore2_service.get_host(),
+                ),
+                # Create an implicit dependency on the migrations
+                # job completing successfully.
+                ContainerEnv(
+                    name="MIGRATIONS_JOB_COMPLETE",
+                    value=migrations_job_complete.apply(lambda b: f"{b}"),
+                ),
+            ],
+            tty=False,
+            securityContext=ContainerSecurityContext(privileged=False),
         ),
-        envs=[
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="DATABASE_URL",
-                value_source=db_url_secret_source,
+        machine_type=machine_type,
+        restart_policy="Always",
+        secret_env=[
+            compute.v1.MetadataItemsItemArgs(
+                key="SSL_CERT_PEM",
+                value=Output.concat(
+                    ssl_cert_pem_secret.id, "/versions/latest"
+                ),
             ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="KEK_URI",
-                value=sessions_encryption_key_uri,
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="SESSIONS_BUCKET",
-                value=sessions_bucket.name,
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="MEDIA_BUCKET",
-                value=media_bucket.name,
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="WHATUPY_CONTROL_GROUPS",
-                value=" ".join(control_groups),
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="WHATUPCORE2_HOST",
-                value=whatupcore2_service.get_host(),
-            ),
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="SSL_CERT_PEM_B64",
-                value_source=ssl_cert_pem_b64_source,
-            ),
-            # Create an implicit dependency on the migrations
-            # job completing successfully.
-            cloudrunv2.ServiceTemplateContainerEnvArgs(
-                name="MIGRATIONS_JOB_COMPLETE",
-                value=migrations_job_complete.apply(lambda b: f"{b}"),
+            compute.v1.MetadataItemsItemArgs(
+                key="DATABASE_URL",
+                value=Output.concat(
+                    db_url_secrets["users"].id, "/versions/latest"
+                ),
             ),
         ],
+        service_account_email=service_account.email,
+        subnet=private_services_network_with_db.self_link,
     ),
     opts=ResourceOptions(
         depends_on=[
             sessions_bucket_perm,
             media_bucket_perm,
             encryption_key_perm,
+            ssl_cert_pem_secret_perm,
         ]
     ),
 )
