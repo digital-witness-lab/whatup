ba190794451da0471c3c2298440ece3adebd23e7
Micha Gorelick <mynameisfiber@gmail.com>
2024-03-12 17:14:32 +0100

    remove unneeded phash_images transfer

--- infrastructure/bigquery.py
+++ infrastructure/bigquery.py
@@ -25,7 +25,6 @@ table_transfers = {
         "messages": {"pk": "id"},
         "media": {"pk": "filename"},
         "donor_messages": {"pk": "id"},
-        "phash_images": {"pk": "filename"},
     },
     "users": {"user_registration_meta": {"pk": "jid"}},
 }
