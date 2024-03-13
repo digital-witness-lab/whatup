5b03e6979107a08fe4ff70bd3a44b02aa7daa67a
Micha Gorelick <mynameisfiber@gmail.com>
2024-03-13 10:33:17 +0100

    Added support for multiple parallel tasks

--- hash-gen/run.sh
+++ hash-gen/run.sh
@@ -2,4 +2,11 @@
 set -eo pipefail
 
 export CLOUDPATHLIB_FILE_CACHE_MODE="close_file"
-exec hash-gen --dataset-id ${PROJECT_ID}.${DATASET_ID}
+
+JOB_COUNT=${CLOUD_RUN_TASK_COUNT:=1}
+JOB_IDX=${CLOUD_RUN_TASK_INDEX:=0}
+
+exec hash-gen \
+    --dataset-id ${PROJECT_ID}.${DATASET_ID} \
+    --job-idx $JOB_IDX \
+    --job-count $JOB_COUNT
