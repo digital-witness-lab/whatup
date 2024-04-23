dc4ab7112f12cb69118b6171ff2dab49540205f8
Micha Gorelick <mynameisfiber@gmail.com>
2024-04-23 12:58:26 +0200

    Automatically create data-viewers and data-editors groups

--- infrastructure/gcp_services.py
+++ infrastructure/gcp_services.py
@@ -28,6 +28,8 @@ for service in [
     "servicenetworking.googleapis.com",
     "sqladmin.googleapis.com",
     "storage.googleapis.com",
+    # google groups
+    "cloudidentity.googleapis.com",
 ]:
     enabled_services.append(
         projects.Service(service, disable_on_destroy=False, service=service)
