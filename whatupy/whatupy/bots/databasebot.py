581e8939bd80995333b71cb040a33086fce80139
Micha Gorelick <mynameisfiber@gmail.com>
2024-05-07 17:29:41 +0200

    Refactor flatten_proto_message oout of databasebot.py

--- whatupy/whatupy/bots/databasebot.py
+++ whatupy/whatupy/bots/databasebot.py
@@ -8,85 +8,16 @@ import dataset
 import grpc
 from cloudpathlib import AnyPath
 from dataset.util import DatasetException
-from google.protobuf.timestamp_pb2 import Timestamp
 from sqlalchemy.sql import func
 
 from .. import utils
 from ..protos import whatupcore_pb2 as wuc
-from . import ArchiveData, BaseBot, BotCommandArgs, MediaType
+from .lib import flatten_proto_message
+from . import ArchiveData, BaseBot
+
 
 logger = logging.getLogger(__name__)
-
-
-IGNORED_FIELDS = set(("originalMessage", "mediaMessage"))
 RECORD_MTIME_FIELD = "record_mtime"
-
-
-def flatten_proto_message(
-    proto_obj,
-    preface_keys=False,
-    prev_keys: T.Optional[T.Iterable[str]] = None,
-    sep="_",
-    skip_keys=None,
-):
-    prev_keys = prev_keys or []
-    skip_keys = skip_keys or set()
-    flat = {}
-    key_list = proto_obj.DESCRIPTOR.fields_by_name.keys()
-    key: str
-    for key in key_list:
-        key_proto = key
-        if key_proto in skip_keys:
-            continue
-        if preface_keys:
-            key = sep.join((*prev_keys, key))
-
-        obj: T.Any = getattr(proto_obj, key_proto)
-        if isinstance(obj, wuc.JID):
-            jid_key = key
-            if "jid" not in jid_key.lower():
-                jid_key = f"{jid_key}_jid"
-            flat[jid_key] = utils.jid_to_str(obj)
-            flat[f"{key}_country_iso"] = obj.userGeocode
-            continue
-        elif isinstance(obj, Timestamp):
-            obj = obj.ToDatetime()
-        elif hasattr(obj, "items"):
-            obj = dict(obj)
-        elif hasattr(obj, "append"):
-            new_obj = []
-            for item in obj:
-                if utils.is_filled_pbuf(item):
-                    item = flatten_proto_message(
-                        item,
-                        preface_keys=preface_keys,
-                        prev_keys=(*prev_keys, key_proto),
-                        sep=sep,
-                        skip_keys=skip_keys,
-                    )
-                elif utils.is_empty_pbuf(item):
-                    continue
-                if item:
-                    new_obj.append(item)
-            obj = new_obj
-        elif key in IGNORED_FIELDS:
-            continue
-
-        if utils.is_filled_pbuf(obj):
-            flat.update(
-                flatten_proto_message(
-                    obj,
-                    preface_keys=preface_keys,
-                    prev_keys=(*prev_keys, key_proto),
-                    sep=sep,
-                    skip_keys=skip_keys,
-                )
-            )
-        elif utils.is_empty_pbuf(obj):
-            continue
-        elif obj:
-            flat[key] = obj
-    return flat
 
 
 class DatabaseBot(BaseBot):
@@ -433,11 +364,11 @@ class DatabaseBot(BaseBot):
         else:
             table: dataset.Table = self.db.get_table("device_group_info")
             group_info_prev = (
-                table.find_one(JID=chat_jid, donor_jid=donor_jid, order_by="-timestamp") or {}
+                table.find_one(JID=chat_jid, donor_jid=donor_jid, order_by="-last_seen") or {}
             )
-            last_update: datetime = group_info_prev.get("timestamp", datetime.min)
-            if not is_archive and last_update + self.group_info_refresh_time > now:
-                self.logger.debug("Is Archive or Too Soon for new group info: %s: %s: %s", last_update, self.group_info_refresh_time, now)
+            last_seen: datetime = group_info_prev.get("last_seen", datetime.min)
+            if not is_archive and last_seen + self.group_info_refresh_time > now:
+                self.logger.debug("Is Archive or Too Soon for new group info: %s: %s: %s", last_seen, self.group_info_refresh_time, now)
                 return
             try:
                 group_info = await self.core_client.GetGroupInfo(chat)
