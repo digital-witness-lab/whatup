596923f49f6a837f49432a0d8cb45096afea9616
Micha Gorelick <mynameisfiber@gmail.com>
2024-10-11 13:14:04 +0200

    bump deploy

--- whatupcore2/run.sh
+++ whatupcore2/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-RAND="iaskdlfjasduifhasiudfhaskdjffdfdfasfhnaks"
+RAND="asjdfhaisudfhsiaughisdgasddfdfdfasfhnaks"
 app_command=$1
 echo "Starting whatupcore2 with command $app_command."
 
